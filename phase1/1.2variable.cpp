9c911bbacce75026d5e7d3e4b4bcfe7519cacc2f
wz <achira@qq.com>
2024-04-12 17:31:23 +0800

    变量

---
+++ phase1/1.2variable.cpp
@@ -0,0 +1,28 @@
+//
+// Created by Teacher on 24-4-12.
+//
+//
+// Created by Teacher on 24-4-12.
+//
+//作用：给一段指定的内存空间起名，方便操作这段内存
+//
+//语法：数据类型 变量名 = 初始值;
+//注意：C++在创建变量时，必须给变量一个初始值，否则会报错
+
+
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//    //变量的定义
+//    //语法：数据类型  变量名 = 初始值
+//
+//    int a = 10;
+//
+//    cout << "a = " << a << endl;
+//
+//    system("pause");
+//
+//    return 0;
+//}
