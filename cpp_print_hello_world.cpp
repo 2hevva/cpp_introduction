f0e4bd778ccb8e7706adfa057a3ff6e8e90a7e15
wz <achira@qq.com>
2024-04-12 16:52:58 +0800

    hello world

---
+++ cpp_print_hello_world.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Teacher on 24-4-12.
+//
+// iostream 顾名思义就是io的流对象库
+#include<iostream>
+//使用namespace可以让库中的对象被直接访问而省略std::前缀，不然每次都需要std::
+using namespace std;
+
+//与c语言相同，程序从main函数开始执行，应该被正确返回0
+int main() {
+    //cout对象是iostream标准输入输出库中std包中的一个对象，使用cout可以标准化打印输出字符到控制台
+    //也包括一些数字可以直接被format为字符串而输出，endl为换行
+    cout << "Hello world" << endl;
+
+    //系统暂停而等待用户按任意键继续
+    system("pause");
+
+    return 0;
+}
