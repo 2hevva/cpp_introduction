0040655f5643aeee7f738916a01dba9fc3fb63f5
achira <achira@qq.com>
2024-04-13 13:45:26 +0800

    更新本地构建

--- phase1/1.3constant.cpp
+++ phase1/1.3constant.cpp
@@ -13,22 +13,22 @@
 //==通常在变量定义前加关键字const==，修饰该变量为常量，不可修改
 
 //1、宏常量
-#define day 7
-#include<iostream>
-using namespace std;
-
-int main() {
-
-    cout << "一周里总共有" << day << "天" << endl;
-    //day = 8;  //报错，宏常量不可以修改
-
-    //2、const修饰变量
-    const int month = 12;
-    cout << "一年里总共有" << month << "个月份" << endl;
-//    month = 24; //报错，常量是不可以修改的
-
-
-    system("pause");
-
-    return 0;
-}
+//#define day 7
+//#include<iostream>
+//using namespace std;
+//
+//int main() {
+//
+//    cout << "一周里总共有" << day << "天" << endl;
+//    //day = 8;  //报错，宏常量不可以修改
+//
+//    //2、const修饰变量
+//    const int month = 12;
+//    cout << "一年里总共有" << month << "个月份" << endl;
+////    month = 24; //报错，常量是不可以修改的
+//
+//
+//    system("pause");
+//
+//    return 0;
+//}
