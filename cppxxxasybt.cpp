363465057623f370bd4738a1c1e2afafa3be61b1
achira <achira@qq.com>
2024-04-13 19:49:40 +0800

    备课c++到6.2

---
+++ cppxxxasybt.cpp
@@ -0,0 +1,284 @@
+//
+// Created by achir on 24-4-13.
+//
+//例子2.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"hello world";
+//    return 0;
+//}
+
+//例子2.2 中文版
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"你好，世界。";
+//    return 0;
+//}
+//练2.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"Happy Spring Festival!";
+//    return 0;
+//}
+
+//练2.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"春眠不觉晓，处处蚊子咬。";
+//    return 0;
+//}
+
+//例3.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"1234 * 5678 = "<<1234 * 5678;
+//    return 0;
+//}
+
+//例3.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"ABCDEFGHIJKLMNOPQR"<<endl;
+//    cout<<"BABCDEFGHIJKLMNOPQ"<<endl;
+//    cout<<"CBABCDEFGHIJKLMNOP"<<endl;
+//    cout<<"DCBABCDEFGHIJKLMNO"<<endl;
+//    cout<<"EDCBABCDEFGHIJKLMN"<<endl;
+//    cout<<"FEDCBABCDEFGHIJKLM"<<endl;
+//    cout<<"GFEDCBABCDEFGHIJKL"<<endl;
+//    cout<<"HGFEDCBABCDEFGHIJK"<<endl;
+//    cout<<"IHGFEDCBABCDEFGHIJ"<<endl;
+//    cout<<"JIHGFEDCBABCDEFGHI"<<endl;
+//    return 0;
+//}
+
+//练3.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"春晓"<<endl;
+//    cout<<"春眠不觉晓，"<<endl;
+//    cout<<"处处蚊子咬。"<<endl;
+//    cout<<"夜来嗡嗡声，"<<endl;
+//    cout<<"脓包知多少。"<<endl;
+//    return 0;
+//}
+
+//练3.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"1+2+3+4="<<1+2+3+4;
+//    return 0;
+//}
+
+
+
+
+//练3.3
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"000"<<endl;
+//    cout<<"001"<<endl;
+//    cout<<"010"<<endl;
+//    cout<<"011"<<endl;
+//    cout<<"100"<<endl;
+//    cout<<"101"<<endl;
+//    cout<<"110"<<endl;
+//    cout<<"111"<<endl;
+//    return 0;
+//}
+
+//例4.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b,t;
+//int main(){
+//    a=20;b=22;
+//    t=a;
+//    a=b;
+//    b=t;
+//    cout<<"a="<<a<<endl<<"b="<<b;
+//    return 0;
+//}
+
+
+//例4.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    int a,b,s;
+//    a=880;
+//    b=500;
+//    s=a*b;
+//    cout<<s;
+//    return 0;
+//}
+
+//练4.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b,t;
+//int main(){
+//    a=10;b=20;
+//    cout<<"a="<<a<<" b="<<b<<endl;
+//    t=a;
+//    a=b;
+//    b=t;
+//    cout<<"a="<<a<<" b="<<b;
+//    return 0;
+//}
+
+//练4.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"5";
+//    return 0;
+//}
+
+
+
+//练4.3
+//#include<bits/stdc++.h>
+//using namespace std;
+//int main(){
+//    cout<<"100"<<endl;
+//    cout<<"101"<<endl;
+//    cout<<"102"<<endl;
+//    return 0;
+//}
+
+
+//练4.4
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b,c,d;
+//int main(){
+//    a = 15;
+//    b = 20;
+//    c = 20;
+//    d = 10;
+//    cout<<((a*b)-(c*d))/10;
+//    return 0;
+//}
+
+
+//例5.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a;
+//int main(){
+//    cin>>a;
+//    cout<<"My height is "<<a<<"cm.";
+//    return 0;
+//}
+
+
+//例5.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int x,y,z;
+//int main(){
+//    cin>>x>>y>>z;
+//    cout<<y;
+//    return 0;
+//}
+
+
+//练5.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int x;
+//int main(){
+//    cin>>x;
+//    cout<<x;
+//    return 0;
+//}
+
+
+
+//练5.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b,t;
+//int main(){
+//    cin>>a>>b;
+//    cout<<"a="<<a<<" b="<<b<<endl;
+//    t=a;
+//    a=b;
+//    b=t;
+//    cout<<"a="<<a<<" b="<<b;
+//    return 0;
+//}
+
+
+//练5.3
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b;
+//int main(){
+//    cin>>a;
+//    b=a*10;
+//    cout<<a<<" "<<b;
+//    return 0;
+//}
+
+//例6.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b,c,d;
+//int main(){
+//    cin>>a>>b>>c>>d;
+//    cout<<a+b+c+d;
+//    return 0;
+//}
+
+
+//例6.2
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b,c;
+//int main(){
+//    cin>>a>>b>>c;
+//    cout<<20-(a+b+c);
+//    return 0;
+//}
+
+
+//例6.3
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b,c;
+//int main(){
+//    cin>>a>>b>>c;
+//    cout<<a+a*b+a*b*c;
+//    return 0;
+//}
+
+
+//练6.1
+//#include<bits/stdc++.h>
+//using namespace std;
+//int a,b;
+//int main(){
+//    cin>>a>>b;
+//    cout<<a*b;
+//    return 0;
+//}
+
+//练6.2
+#include<bits/stdc++.h>
+using namespace std;
+int a,b;
+int main(){
+    cin>>a>>b;
+    cout<<"总的棵数："<<a*b;
+    return 0;
+}
